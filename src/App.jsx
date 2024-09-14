c06b99e7451918a5077a3d614b005f734d343961
Margan Das <152724498+Mr-Nobody003@users.noreply.github.com>
2024-09-14 11:12:05 +0000

    main_page

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
 import react ,{ useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class="flex flex-row pt-4 font-mono font-bold">
+        <div class="flex felx-col ">
+          <ul class="space-y-5 mx-8">
+            <li><a href="" class="text-red-600 text-6xl hover:text-red-500 font"> <span class="text-white text-center text-xl">&#8226;</span>PLAY</a> </li>
+            <li><a href="" class="text-white text-2xl ">&#8226; CAREER</a> </li>
+            <li><a href="" class="text-white text-2xl">&#8226; BATTLEPASS</a> </li>
+            <li><a href="" class="text-white text-2xl">&#8226; COLLECTION</a> </li>
+            <li><a href="" class="text-white text-2xl">&#8226; AGENTS</a> </li>
+            <li><a href="" class="text-white  text-2xl">&#8226; STORE</a> </li>
+            
+          </ul>
+        </div>
+        <div class="text-white">
+          
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
