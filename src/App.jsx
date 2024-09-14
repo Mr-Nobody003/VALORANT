ce865f2c02c34dbb9717ca8880f088a985ecf22e
WilliamHails <153358752+WilliamHails@users.noreply.github.com>
2024-09-14 12:19:22 +0000

    Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import react ,{ useState } from 'react'
+import Mainmenu from './components/Mainmenu'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
@@ -8,20 +9,11 @@ function App() {
 
   return (
     <>
-      <div class="flex flex-row pt-4 font-mono font-bold">
-        <div class="flex felx-col ">
-          <ul class="space-y-5 mx-8">
-            <li><a href="" class="text-red-600 text-6xl hover:text-red-500 font"> <span class="text-white text-center text-xl">&#8226;</span>PLAY</a> </li>
-            <li><a href="" class="text-white text-2xl ">&#8226; CAREER</a> </li>
-            <li><a href="" class="text-white text-2xl">&#8226; BATTLEPASS</a> </li>
-            <li><a href="" class="text-white text-2xl">&#8226; COLLECTION</a> </li>
-            <li><a href="" class="text-white text-2xl">&#8226; AGENTS</a> </li>
-            <li><a href="" class="text-white  text-2xl">&#8226; STORE</a> </li>
-            
-          </ul>
-        </div>
-        <div class="text-white">
-          
+      <div class=" h-screen content-center">
+        <div class=" self-center">
+          <Mainmenu/>
+
+        
         </div>
       </div>
 
@@ -30,3 +22,38 @@ function App() {
 }
 
 export default App
+
+
+
+
+
+
+// <div class="flex flex-row pt-4 font-mono font-bold">
+//         <div class="flex ">
+          
+//           {/* <div>
+//             <div class="flex flex-row ">
+//             <span class="text-white text-center content-center text-2xl ">&#8226;</span>
+//             <a href="" class="text-red-600 text-6xl hover:text-red-500 font"> PLAY</a>
+//             </div>
+//           </div>
+//            */}
+          
+          
+          
+          
+//           <ul class="space-y-5 mx-8">
+//             <li class="flex flex-row space-x-2"><span class="text-white text-center content-center text-2xl ">&#8226;</span><a href="" class="text-red-600 text-6xl hover:text-red-500 font"> PLAY</a> </li>
+//             <li><a href="" class="text-white text-2xl ">&#8226; CAREER</a> </li>
+//             <li><a href="" class="text-white text-2xl">&#8226; BATTLEPASS</a> </li>
+//             <li><a href="" class="text-white text-2xl">&#8226; COLLECTION</a> </li>
+//             <li><a href="" class="text-white text-2xl">&#8226; AGENTS</a> </li>
+//             <li><a href="" class="text-white  text-2xl">&#8226; STORE</a> </li>
+            
+//           </ul>
+//         </div>
+//         <div class="text-white">
+          
+//         </div>
+//       </div>
+
---
+++ src/components/Mainmenu.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Mainmenu = () => {
+  return (
+    <>
+      <div class="flex flex-row pt-4 font-mono font-bold">
+        <div class="flex ">
+          
+          <ul class="space-y-5 mx-8">
+            <li class="flex flex-row space-x-2"><span class="text-white text-center content-center text-2xl ">&#8226;</span><a href="" class="text-red-600 text-6xl hover:text-red-500 font"> PLAY</a> </li>
+            <li><a href="" class="text-white text-2xl ">&#8226; CAREER</a> </li>
+            <li><a href="" class="text-white text-2xl">&#8226; BATTLEPASS</a> </li>
+            <li><a href="" class="text-white text-2xl">&#8226; COLLECTION</a> </li>
+            <li><a href="" class="text-white text-2xl">&#8226; AGENTS</a> </li>
+            <li><a href="" class="text-white  text-2xl">&#8226; STORE</a> </li>
+            
+          </ul>
+        </div>
+        <div class="text-white">
+          
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default Mainmenu
