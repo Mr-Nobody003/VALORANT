db62687d9a8d091dfc236bd34e7b2994b22ff342
Margan Das <152724498+Mr-Nobody003@users.noreply.github.com>
2024-09-15 14:24:49 +0000

    ig

--- src/components/Mainmenu.jsx
+++ src/components/Mainmenu.jsx
@@ -7,12 +7,12 @@ const Mainmenu = () => {
         <div class="flex ">
           
           <ul class="space-y-5 mx-8">
-            <li class="flex flex-row space-x-2"><span class="text-white text-center content-center text-sm ">&#9670;</span><a href="" class="text-red-600 text-6xl hover:text-red-500 font"> PLAY</a> </li>
-            <li><span class="text-white text-center content-center text-sm ">&#9670;</span><a href="" class="text-white text-2xl "> CAREER</a> </li>
-            <li><span class="text-white text-center content-center text-sm ">&#9670;</span><a href="" class="text-white text-2xl"> BATTLEPASS</a> </li>
-            <li><span class="text-white text-center content-center text-sm ">&#9670;</span><a href="" class="text-white text-2xl"> COLLECTION</a> </li>
-            <li><span class="text-white text-center content-center text-sm ">&#9670;</span><a href="" class="text-white text-2xl"> AGENTS</a> </li>
-            <li><span class="text-white text-center content-center text-sm ">&#9670;</span><a href="" class="text-white  text-2xl"> STORE</a> </li>
+            <li class="flex flex-row space-x-2"><span class="text-white text-center content-center text-sm">&#9670;</span><a href="" class="text-red-600 text-6xl hover:text-green-400 hover:text-7xl "> PLAY</a> </li>
+            <li><span class="text-white text-center content-center text-sm ">&#9670;</span><a href="" class="text-white text-2xl  hover:text-green-400 hover:text-3xl"> CAREER</a> </li>
+            <li><span class="text-white text-center content-center text-sm ">&#9670;</span><a href="" class="text-white text-2xl hover:text-green-400 hover:text-3xl"> BATTLEPASS</a> </li>
+            <li><span class="text-white text-center content-center text-sm ">&#9670;</span><a href="" class="text-white text-2xl hover:text-green-400 hover:text-3xl"> COLLECTION</a> </li>
+            <li><span class="text-white text-center content-center text-sm ">&#9670;</span><a href="" class="text-white text-2xl hover:text-green-400 hover:text-3xl"> AGENTS</a> </li>
+            <li><span class="text-white text-center content-center text-sm ">&#9670;</span><a href="" class="text-white  text-2xl hover:text-green-400 hover:text-3xl"> STORE</a> </li>
             
           </ul>
         </div>
