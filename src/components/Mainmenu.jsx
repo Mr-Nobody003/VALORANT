a4699b528c9d79f57c79d5bba98bcacb5ecb83bd
Margan Das <152724498+Mr-Nobody003@users.noreply.github.com>
2024-09-15 16:05:27 +0000

    hover_rotation

--- src/components/Mainmenu.jsx
+++ src/components/Mainmenu.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 const Mainmenu = () => {
   return (
     <>
-      <div class="flex flex-row pt-4 font-mono font-bold">
+      <div class="flex flex-row pt-4 scale-y-125 font-bold">
         <div class="flex ">
           
-          <ul class="space-y-5 mx-8">
-            <li class="flex flex-row space-x-2"><span class="text-white text-center content-center text-sm">&#9670;</span><a href="" class="text-red-600 text-6xl hover:text-green-400 hover:text-7xl "> PLAY</a> </li>
-            <li><span class="text-white text-center content-center text-sm ">&#9670;</span><a href="" class="text-white text-2xl  hover:text-green-400 hover:text-3xl"> CAREER</a> </li>
-            <li><span class="text-white text-center content-center text-sm ">&#9670;</span><a href="" class="text-white text-2xl hover:text-green-400 hover:text-3xl"> BATTLEPASS</a> </li>
-            <li><span class="text-white text-center content-center text-sm ">&#9670;</span><a href="" class="text-white text-2xl hover:text-green-400 hover:text-3xl"> COLLECTION</a> </li>
-            <li><span class="text-white text-center content-center text-sm ">&#9670;</span><a href="" class="text-white text-2xl hover:text-green-400 hover:text-3xl"> AGENTS</a> </li>
-            <li><span class="text-white text-center content-center text-sm ">&#9670;</span><a href="" class="text-white  text-2xl hover:text-green-400 hover:text-3xl"> STORE</a> </li>
+          <ul class="space-y-1 mx-8">
+            <li class="flex flex-row space-x-2 group h-20"><span class="text-white text-center content-center text-sm transition group-hover:rotate-180 group-hover:text-green-400 -mt-4 group-hover:text-lg">◆</span><a href="" class="text-red-600 text-6xl hover:text-green-400"> PLAY</a> </li>
+            <li class="flex flex-row space-x-2 group  h-10"><span class="text-white text-center content-center text-sm transition group-hover:rotate-180 group-hover:text-green-400 -mt-2 group-hover:text-lg">◆</span><a href="" class="text-white text-2xl  hover:text-green-400 "> CAREER</a> </li>
+            <li class="flex flex-row space-x-2 group h-10"><span class="text-white text-center content-center text-sm transition group-hover:rotate-180 group-hover:text-green-400 -mt-2 group-hover:text-lg">◆</span><a href="" class="text-white text-2xl hover:text-green-400 "> BATTLEPASS</a> </li>
+            <li class="flex flex-row space-x-2 group h-10"><span class="text-white text-center content-center text-sm transition group-hover:rotate-180 group-hover:text-green-400 -mt-2 group-hover:text-lg">◆</span><a href="" class="text-white text-2xl hover:text-green-400 "> COLLECTION</a> </li>
+            <li class="flex flex-row space-x-2 group h-10"><span class="text-white text-center content-center text-sm transition group-hover:rotate-180 group-hover:text-green-400 -mt-2 group-hover:text-lg">◆</span><a href="" class="text-white text-2xl hover:text-green-400 "> AGENTS</a> </li>
+            <li class="flex flex-row space-x-2 group h-10"><span class="text-white text-center content-center text-sm transition group-hover:rotate-180 group-hover:text-green-400 -mt-2 group-hover:text-lg">◆</span><a href="" class="text-white  text-2xl hover:text-green-400 "> STORE</a> </li>
             
           </ul>
         </div>
