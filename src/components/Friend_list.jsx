b50c1336369135bd2130172c05790b56184288e2
Margan Das <152724498+Mr-Nobody003@users.noreply.github.com>
2024-09-28 23:46:31 +0000

    Premieer_components _added

--- src/components/Friend_list.jsx
+++ src/components/Friend_list.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Friend_list = () => {
   return (
-    <div class="flex flex-col text-white z-20 border-white border-solid border-2">
+    <div class="z-50 pt-10 ">
+    <div class="flex flex-col text-white z-50 border-white border-solid border-2">
       <div class="flex  border-white border-solid border-2">fr1</div>
       <div class="flex  border-white border-solid border-2">fr2</div>
+    </div>
     </div>
   )
 }
