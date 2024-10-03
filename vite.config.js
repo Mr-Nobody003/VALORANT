bd65a8092c7e3ea34e7c0c3a53678900c42e7c75
Margan Das <152724498+Mr-Nobody003@users.noreply.github.com>
2024-10-03 22:42:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:process.env.NODE_ENV === 'production'
-    ? '/VALORANT/' // for GitHub Pages
-    : '/', // for Vercel
+  base: process.env.VERCEL === 'true'
+    ? '/' // No base path for Vercel
+    : '/VALORANT/', // Base path for GitHub Pages
 })
