f492025a2d5da47b3fba3b27c5a5177791e70f4b
Margan Das <152724498+Mr-Nobody003@users.noreply.github.com>
2024-10-03 22:34:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/VALORANT/"
+  base:process.env.NODE_ENV === 'production'
+    ? '/VALORANT/' // for GitHub Pages
+    : '/', // for Vercel
 })
