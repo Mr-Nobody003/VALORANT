d52c8877cad59c1e6a623c442439224b9a713272
Margan Das <152724498+Mr-Nobody003@users.noreply.github.com>
2024-10-03 21:44:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
-import Main_page from './Pages/Main_page'
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Main_page from './Pages/Main_page';
 import Play_page from './Pages/Play_page';
 import Premieer_page from './Pages/Premieer_page';
 import Collection_page from './Pages/Collection_page';
@@ -13,48 +14,58 @@ function App() {
   const [currentPage, setCurrentPage] = useState('Main_page');
   const [showLogoPage, setShowLogoPage] = useState(true);
 
-  const showBackButton = currentPage === 'Play_page' || currentPage === 'Premieer_page' || currentPage === 'Collection_page' || currentPage === 'Battlepass_page' || currentPage === 'Agent_page' || currentPage === 'Store_page' || currentPage === 'Career_page';
+  const showBackButton =
+    currentPage === 'Play_page' ||
+    currentPage === 'Premieer_page' ||
+    currentPage === 'Collection_page' ||
+    currentPage === 'Battlepass_page' ||
+    currentPage === 'Agent_page' ||
+    currentPage === 'Store_page' ||
+    currentPage === 'Career_page';
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
   };
 
-  // const handleBackToMainMenu = () => {
-  //   setCurrentPage('Main_page');
-  // };
   const handleLogoClick = () => {
     setShowLogoPage(false);
   };
 
   return (
-    <>
+    <Router basename="/VALORANT"> {/* Set basename here */}
       {showLogoPage ? (
         // Logo page content
         <div
           className="w-screen h-screen overflow-hidden cursor-pointer"
           onClick={handleLogoClick}
         >
-          <img class="object-fill" src="https://raw.githubusercontent.com/Mr-Nobody003/VALORANT/main/src/assets/valo_START.png" alt="Logo" />
+          <img
+            className="object-fill"
+            src="https://raw.githubusercontent.com/Mr-Nobody003/VALORANT/main/src/assets/valo_START.png"
+            alt="Logo"
+          />
         </div>
       ) : (
         <>
-          <Nav onPageChange={handlePageChange} showBackButton={showBackButton} current_Page={currentPage} />
-          {currentPage === 'Main_page' && <Main_page onPageChange={handlePageChange} />}
-          {currentPage === 'Play_page' && <Play_page />}
-          {currentPage === 'Premieer_page' && <Premieer_page />}
-          {currentPage === 'Collection_page' && <Collection_page />}
-          {currentPage === 'Career_page' && <Career_page />}
-          {currentPage === 'Agent_page' && <Agent_page />}
-          {currentPage === 'Battlepass_page' && <Battlepass_page />}
-          {currentPage === 'Store_page' && <Store_page />}
+          <Nav
+            onPageChange={handlePageChange}
+            showBackButton={showBackButton}
+            current_Page={currentPage}
+          />
+          <Routes>
+            <Route path="/" element={<Main_page onPageChange={handlePageChange} />} />
+            <Route path="/play" element={<Play_page />} />
+            <Route path="/premieer" element={<Premieer_page />} />
+            <Route path="/collection" element={<Collection_page />} />
+            <Route path="/career" element={<Career_page />} />
+            <Route path="/agent" element={<Agent_page />} />
+            <Route path="/battlepass" element={<Battlepass_page />} />
+            <Route path="/store" element={<Store_page />} />
+          </Routes>
         </>
       )}
-    </>
+    </Router>
   );
 }
 
-export default App
-
-
-
-
+export default App;
