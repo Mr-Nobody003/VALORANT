9002344a197dd0ca0d903bb1512c38d68318229f
Margan Das <152724498+Mr-Nobody003@users.noreply.github.com>
2024-10-03 18:24:22 +0000

    vite_edited

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: process.env.VERCEL === 'true'
-//     ? '' // No base path for Vercel
-//     : '/VALORANT/', // Base path for GitHub Pages
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
