7cd072688b20c1856022da0b42f89d1e7101a03d
Margan Das <152724498+Mr-Nobody003@users.noreply.github.com>
2024-10-25 19:24:56 +0530

    Main Page vid added

--- src/Pages/Main_page.jsx
+++ src/Pages/Main_page.jsx
@@ -2,15 +2,22 @@ import React ,{ useState } from 'react'
 import Mainmenu from '../components/Mainmenu'
 import Info from '../components/Info'
 import Friend_list from '../components/Friend_list'
-
+import Home_vid from "../assets/video/Homescreen.mp4";
 const Main_page = ({ onPageChange }) =>  {
   // const [count, setCount] = useState(0)
  //  const [showLogoPage, setShowLogoPage] = useState(true);
   return (
-      <div class="">
+      <>
         {/* <div class="justify-between">
           <Nav/>
         </div> */}
+        <video
+        src={Home_vid}
+        autoPlay
+        muted
+        loop
+        className="fixed top-0 left-0 w-screen h-screen object-cover -z-10"
+      />
         <div class="justify-between">
           <div class="absolute left-0 top-1/3 ">
             <Mainmenu  onPageChange={onPageChange}/>
@@ -25,7 +32,7 @@ const Main_page = ({ onPageChange }) =>  {
           </div>
         </div>
         
-      </div>
+      </>
   )
 }
 
