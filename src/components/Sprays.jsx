38e773ed82b737f50946ca08fff740990e245731
Subhendu Datta <subhendudatta04@gmail.com>
2024-12-27 16:22:22 +0000

    Spray Done

--- src/components/Sprays.jsx
+++ src/components/Sprays.jsx
@@ -1,46 +1,49 @@
 import React from 'react'
+import S1 from "../assets/sprays/S1.png"
+import S2 from "../assets/sprays/S2.png"
+import S3 from "../assets/sprays/S3.png"
+import S4 from "../assets/sprays/S4.png"
+
 const Sprays = () => {
   return (
-    <div className='flex flex-col justify-center items-center'>
-      <div className="flex item-center font-Oswald text-white text-[23px]">SPRAYS</div>
-      {/* <!-- Outer Circle --> */}
-      <div class="relative w-[220px] h-[220px] rounded-full border-2 border-gray-500 opacity-75 ">
+    <div class="flex relative  items-center justify-center bg-transparent">
+      <div class="flex relative z-10 h-[200px] w-[200px] rotate-45 rounded-[100%]">
+        <div class="flex absolute top-0 h-[100px] w-[100px] cursor-pointer rounded-tl-[100%] border-r-[1px] border-white from-teal-400 from-[10%] via-transparent via-[80%] to-transparent transition-transform duration-100 hover:scale-110 hover:border-teal-400 hover:bg-gradient-to-tl">
+          <div className="flex absolute top-0   p-0  bg-transperent flex items-center justify-center  hover:transperent hover:scale-105 transition duration-300 " >
+            <img src={"https://static.wikia.nocookie.net/valorant/images/6/69/Too_Early_for_This_Spray.gif/"} alt="Top" className="w-16 h-16 object-contain Text-white -rotate-45  mt-[29px] ml-[22px]"></img>
+          </div></div>
+        {/* TOP */}
+        <div class="flex absolute left-1/2 top-0 h-[100px] w-[100px] cursor-pointer rounded-tr-[100%] border-b-[1px] border-white from-teal-400 from-[10%] via-transparent via-[80%] to-transparent transition-transform duration-100 hover:scale-110 hover:border-teal-400 hover:bg-gradient-to-tr">
+          <div className="absolute top-0   p-0  bg-transperent flex items-center justify-center  hover:transperent hover:scale-105 transition duration-300 " >
+            <img src={"https://static.wikia.nocookie.net/valorant/images/1/11/Wingman_Wiggle_Spray.gif/"} alt="Right" className="w-14 h-14 object-contain -rotate-45  mt-[29px] ml-[13px] "></img>
 
-        {/* <!-- Diagonal Line 1 --> */}
-        <div class="absolute top-0 left-1/2 w-px h-full bg-white transform -translate-x-1/2 rotate-45"></div>
 
-        {/* <!-- Diagonal Line 2 --> */}
-        <div class="absolute top-0 left-1/2 w-px h-full bg-white transform -translate-x-1/2 -rotate-45"></div>
-        <div className="flex items-center justify-center bg-transperent">
-          {/* <!-- Circle Container --> */}
-          <div className="relative w-[210px] h-[210px] bg-transperent rounded-full flex items-center justify-center p-0 m-0">
+          </div></div>
+        {/* Right */}
+        <div class="flex absolute bottom-0 h-[100px] w-[100px] cursor-pointer rounded-bl-[100%] border-t-[1px] border-white from-teal-400 from-[10%] via-transparent via-[80%] to-transparent transition-transform duration-100 hover:scale-110 hover:border-teal-400 hover:bg-gradient-to-bl">
 
-            {/* <!-- Top Section --> */}
-            <div className="absolute top-0 left-[75px]  p-0  bg-transperent flex items-center justify-center  hover:transperent hover:scale-150 transition duration-300 " >
-              <img src={"https://static.wikia.nocookie.net/valorant/images/6/69/Too_Early_for_This_Spray.gif/"} alt="Top" className="w-16 h-16 object-fill "></img>
-            </div>
+          <div className="absolute top-0  p-0  bg-transperent flex items-center justify-center  hover:transperent hover:scale-105 transition duration-300 " >
+            <img src={"https://static.wikia.nocookie.net/valorant/images/b/b0/Bubble_Pop_Spray.gif"} alt="Left" className="w-14 h-14 object-contain -rotate-45 mt-2 ml-6"></img>
 
-            {/* <!-- Right Section --> */}
-            <div className="absolute right-0 top-[75px]  p-0  bg-transperent flex items-center justify-center  hover:transperent hover:scale-150 transition duration-300">
-              <img src={"https://static.wikia.nocookie.net/valorant/images/1/11/Wingman_Wiggle_Spray.gif/"} alt="Right" className="w-16 h-16 object-fill"></img>
-            </div>
 
-            {/* <!-- Bottom Section --> */}
-            <div className="absolute bottom-0 left-[75px]   p-0  bg-transperent flex items-center justify-center  hover:transperent hover:scale-150 transition duration-300">
-              <img src={"https://static.wikia.nocookie.net/valorant/images/2/24/Sentinel_Slide_Spray.gif/"} alt="Bottom" className="w-16 h-16 object-fill"></img>
-            </div>
 
-            {/* <!-- Left Section --> */}
-            <div className="absolute left-0 top-[75px]  p-0  bg-transperent flex items-center justify-center  hover:transperent hover:scale-150 transition duration-300">
-              <img src={"https://static.wikia.nocookie.net/valorant/images/b/b0/Bubble_Pop_Spray.gif"} alt="Left" className="w-16 h-16 object-fill"></img>
-            </div>
-            <div class="absolute top-1/2 left-1/2 w-14 h-14 rounded-full bg-gray-800 opacity-70 transform -translate-x-1/2 -translate-y-1/2 border"></div>
-            <div class="absolute top-1/2 left-1/2 w-6 h-6 rounded-full bg-gray-400 opacity-30 transform -translate-x-1/2 -translate-y-1/2  border-none"></div>
           </div>
         </div>
+        {/* LEft */}
+        <div class="flex absolute bottom-0 left-1/2 h-[100px] w-[100px] cursor-pointer rounded-br-[100%] border-l-[1px] border-white from-teal-400 from-[10%] via-transparent via-[80%] to-transparent transition-transform duration-100 hover:scale-110 hover:border-teal-400 hover:bg-gradient-to-br">
+
+          <div className="absolute top-0  p-0  bg-transperent flex items-center justify-center  hover:transperent hover:scale-105 transition duration-300 " >
+            <img src='https://static.wikia.nocookie.net/valorant/images/b/b0/Bubble_Pop_Spray.gif' alt='Bottom' className='w-14 h-14 object-contain mt-2 ml-3 -rotate-45'></img>
+
+
+          </div></div>
+        {/* BOTTOM */}
+      </div>
+      <div class="flex absolute z-20 h-[60px] w-[60px] rounded-full border-[1px] border-white bg-inherit"></div>
+      <div class="flex absolute h-[120px] w-[120px] rounded-full border-[1px] border-slate-500"></div>
+      <div class="flex absolute h-[170px] w-[170px] rounded-full border-[1px] border-slate-500">
       </div>
     </div>
-
   )
 }
 
